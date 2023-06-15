c52f1e842bbb70f09ccef1cfec98b698a1462c0b
Omar EL ATMANI <contact@omaghd.com>
2023-06-15 12:49:51 +0100

    Bind repositories interfaces with implementations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,19 @@
 
 namespace App\Providers;
 
+use App\Repositories\Category\CategoryRepository;
+use App\Repositories\Category\ICategoryRepository;
+use App\Repositories\Product\IProductRepository;
+use App\Repositories\Product\ProductRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public array $bindings = [
+        ICategoryRepository::class => CategoryRepository::class,
+        IProductRepository::class  => ProductRepository::class,
+    ];
+
     /**
      * Register any application services.
      */
