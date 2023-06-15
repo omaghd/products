4a4dbb5ab08566ed13284eb8f965649c3e8817ae
Omar EL ATMANI <contact@omaghd.com>
2023-06-15 12:31:22 +0100

    Create repository and its interface for Product

---
+++ app/Repositories/Product/IProductRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repositories\Product;
+
+use App\Models\Product;
+
+interface IProductRepository
+{
+    public function all(): array;
+
+    public function find(int $id): Product;
+
+    public function create(array $data): Product;
+
+    public function syncCategories(int $id, array $categoryIds): void;
+
+    public function update(int $id, array $data): Product;
+
+    public function delete(int $id): void;
+}
---
+++ app/Repositories/Product/ProductRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repositories\Product;
+
+use App\Models\Product;
+
+class ProductRepository implements IProductRepository
+{
+    public function all(): array
+    {
+        return Product::all()->toArray();
+    }
+
+    public function create(array $data): Product
+    {
+        return Product::create($data);
+    }
+
+    public function syncCategories(int $id, array $categoryIds): void
+    {
+        $product = $this->find($id);
+        $product->categories()->sync($categoryIds);
+    }
+
+    public function find(int $id): Product
+    {
+        return Product::findOrFail($id);
+    }
+
+    public function update(int $id, array $data): Product
+    {
+        $category = $this->find($id);
+        $category->update($data);
+
+        return $category;
+    }
+
+    public function delete(int $id): void
+    {
+        $category = $this->find($id);
+        $category->delete();
+    }
+}
